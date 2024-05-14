65d8fad5471658114cd325ab9985976774467a2a
Josemarie <153520174+Josemarieanto@users.noreply.github.com>
2024-05-14 07:49:46 -0700

    Add files via upload

---
+++ JAVASCRIPT/loop.js
@@ -0,0 +1,151 @@
+// problem: 1
+
+// document.write("The first 10 natural numbers:")
+// for(let i = 1; i <= 10; i ++){
+//     document.write(i);
+// }
+
+// problem: 2
+
+// document.write("The sum of the first 10 natural numbers is :")
+// let i, sum = 0;
+// for(i = 1; i <= 10; i ++){
+//     sum += i;
+// }
+// document.write("\n",sum)
+
+// problem: 3
+// n = parseInt(prompt("Enter the natural number greater than 0: "))
+// document.write(`The sum of the first ${n} natural numbers is :`)
+// let i, sum = 0;
+// for(i = 1; i <= n; i ++){
+//     sum += i;
+// }
+// document.write("\n",sum)
+
+
+// problem: 4
+// document.write(`The input numbers are :`)
+// n = parseInt(prompt("Enter the natural number 1: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 2: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 3: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 4: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 5: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 6: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 7: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 8: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 9: "))
+// document.write(n)
+// n = parseInt(prompt("Enter the natural number 10: "))
+// document.write(n)
+// let i, sum = 0, average;
+// for(i = 1; i <= n; i ++){
+//     sum += i;
+//     average = sum / 10
+// }
+// document.write(`<br>`)
+// document.write(`The sum of 10 numbers is :${sum}<br>`)
+// document.write(`The average of 10 numbers is :${average}`)
+
+// problem: 5
+
+// n = parseInt(prompt("Enter the number: "))
+// let i, cube;
+// for(i = 1; i <= n; i++){
+//     cube = i ** 3;
+//     document.write(`the cube of ${i} ${cube} <br>`)
+// }
+
+// problem: 6
+
+// n = parseInt(prompt("Enter the number: "))
+// let i, table;
+// for(i = 1; i <= 10; i++){
+//     table = n * i;
+//     document.write(`${n} x ${i}  =  ${table} <br>`)
+// }
+
+// problem: 7
+
+// n = parseInt(prompt("Enter the number from 1: "))
+// let i,j, table;
+// for(i = 1; i <= 10; i++){
+//     for(j = 1; j <= n; j ++){
+//         table = j * i;
+//         document.write(`${j} x ${i}  =  ${table} <br>`)    
+//     }
+//     document.write(`<br>`)
+// }
+
+
+//  problem : 8
+
+
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+// v1 = parseInt(prompt("Enter the number: "))
+
+// document.write(`The odd numbers are:`)
+// let i , sum = 0;
+// for(i = 1; i <= 10; i ++){
+//     document.write(`${2 * i - 1} \n`);
+//     sum += 2 * i - 1;
+// }
+// document.write(`<br>The sum of odd numbers is: ${sum}`)
+
+// problem : 9
+
+// for(let i = 1; i < 5; i++){
+//     for(let j = 1; j <= i; j++){
+//         document.write(`*`);
+//     }    
+//     document.write("<br>");
+// }
+
+// problem : 10
+
+// for(let i = 1; i < 5; i++){
+//     for(let j = 1; j <= i; j++){
+//         document.write(j);
+//     }    
+//     document.write("<br>");
+// }
+
+// problem : 11
+
+// for(let i = 1; i < 5; i++){
+//     for(let j = 1; j <= i; j++){
+//         document.write(i);
+//     }    
+//     document.write("<br>");
+// }
+
+// problem : 12
+
+// let i, k = 1
+
+// for(i = 1; i < 5; i++){
+//     for(j = 1; j <= i; j++){
+//         document.write(`${k++}\n`);
+//     }    
+//     document.write("<br>");
+// }
+
+
+
+
