2176812ac8c6a4e91d5156c2b2ae4026024c443d
Josemarie <153520174+Josemarieanto@users.noreply.github.com>
2024-05-26 17:07:37 +0530

    Add files via upload

---
+++ JAVASCRIPT/task/index.js
@@ -0,0 +1,54 @@
+// let n = parseInt(prompt("Enter the number"));
+// let a = 0;
+// let b = 0;
+// let c = 0;
+// let d = 0;
+// let e = 0;
+// let f = 0;
+// let g = 0;
+// let h = 0;
+// let j = 0;
+// let k = 0;
+
+// let s = n.toString().split("");
+
+// console.log(s);
+
+// for(let i of s) {
+//     if(i == 0){
+//         a += 1;   
+//     }else if(i == 1){
+//         b += 1;
+//     }else if(i == 2){
+//         c += 1;
+//     }else if(i == 3){
+//         d += 1;
+//     }else if(i == 4){
+//         e += 1;
+//     }else if(i == 5){
+//         f += 1;
+//     }else if(i == 6){
+//         g += 1;
+//     }else if(i == 7){
+//         h += 1;
+//     }else if(i == 8){
+//         j += 1;
+//     }else if(i == 9){
+//         k += 1;
+//     }else{
+//         console.log("invalid input")
+//     }
+// }
+
+// document.write(`Frequency of 0: ${a}<br/>`)
+// document.write(`Frequency of 1: ${b}<br/>`)
+// document.write(`Frequency of 2: ${c}<br/>`)
+// document.write(`Frequency of 3: ${d}<br/>`)
+// document.write(`Frequency of 4: ${e}<br/>`)
+// document.write(`Frequency of 5: ${f}<br/>`)
+// document.write(`Frequency of 6: ${g}<br/>`)
+// document.write(`Frequency of 7: ${h}<br/>`)
+// document.write(`Frequency of 8: ${j}<br/>`)
+// document.write(`Frequency of 9: ${k}<br/>`)
+
+
